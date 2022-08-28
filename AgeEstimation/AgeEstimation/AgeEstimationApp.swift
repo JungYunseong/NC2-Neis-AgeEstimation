46abc98f1914ba2322a4c7e2fc852cd6306afdf7
JungYunseong <cysseo1@naver.com>
2022-08-28 17:42:58 +0900

    [Feat] 프로젝트 파일 생성

---
+++ AgeEstimation/AgeEstimation/AgeEstimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  AgeEstimationApp.swift
+//  AgeEstimation
+//
+//  Created by Jung Yunseong on 2022/08/28.
+//
+
+import SwiftUI
+
+@main
+struct AgeEstimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
